f9f4a529d50dbc246a70222d626157af079b1b2a
xianjiezh <904292766@qq.com>
2018-09-20 20:26:04 +0800

    添加了使用方法

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # go-server
 基于 go 语言的 web 服务器
+
+## go 语言起步
+运行方法
+命令行输入
+```
+go build server.go
+```
+会生成一个可执行文件
+
+
+windows 下运行
+```
+./server.exe
+```
+可在终端窗口执行该程序
--- server.go
+++ server.go
@@ -23,7 +23,7 @@ func main() {
     http.HandleFunc("/", sayHello) //设置访问的路由
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
-		log.Fatal("Lis  tenAndServe: ", err)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
