e33f9cd11a973775af0f455f95d84957b1de221b
xianjiezh <904292766@qq.com>
2018-11-08 15:36:45 +0800

    第一个web server

--- server.go
+++ server.go
@@ -4,14 +4,23 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (mh MyHandler) ServeHTTP(w http.ResponseWriter, r * http.Request) {
+	if r.URL.Path == "/abc" {
+		w.Write([]byte("abc"))
+		return 
+	}
+	if r.URL.Path == "/xyz" {
+		w.Write([]byte("xyz"))
+		return
+	}
+	w.Write([]byte("index"))
+}
+
 func main()  {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("LaLaLaLa"))
-	})
-	http.HandleFunc("/abc", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("abccccccc"))
-	})
-	if err := http.ListenAndServe(":12345", nil); err != nil {
-		fmt.Println("start http errors", err)
+	http.Handle("/", MyHandler{})
+	if err := http.ListenAndServe(":12345", MyHandler{}); err != nil {
+		fmt.Println("start http err", err)
 	}
 }
